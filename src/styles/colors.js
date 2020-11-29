3671749fc1505339bd8cca815b7f069d896c9806
Mark Gun <58705522+mgunsd@users.noreply.github.com>
2020-11-29 21:34:15 +0000

    Dark colour updateed

--- src/styles/colors.js
+++ src/styles/colors.js
@@ -1,4 +1,10 @@
 export const colors = {
-  dark: '#2c2a2a',
+  dark: '#747D71',
   light: '#f2efe8',
 };
+
+//version 1
+// export const colors = {
+//   dark: '#2c2a2a',
+//   light: '#f2efe8',
+// };
